ac9d9e98af24bb367d773157d94c9d3accf94c5e
ParasharDeb <parashardeb11@gmail.com>
2025-08-25 18:05:11 +0530

    added /me endpoint

--- src/profile.ts
+++ src/profile.ts
@@ -1,3 +1,19 @@
 import express, { Router } from "express"
+import { authmiddleware } from "./middleware";
+import { AuthenticatedRequest } from "./interfaces";
+import { prismaclient } from "./db";
 export const Profileroutes:Router=express.Router();
-Profileroutes.get("/me",auth)
+Profileroutes.get("/me",authmiddleware,async(req,res)=>{
+    const userId=(req as unknown as AuthenticatedRequest).userId;
+    if(!userId){
+        return res.json({message:"You are not signed in"})
+    }
+    const user = await prismaclient.user.findFirst({
+        where:{
+            id:userId
+        }
+    })
+    res.json({
+        user
+    })
+})
